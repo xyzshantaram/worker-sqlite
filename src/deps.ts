37d9a069abcd5c8d70b5631e2c90cd26aa0f151a
Siddharth Singh <me@shantaram.xyz>
2022-10-22 04:34:11 +0530

    update deps

--- src/deps.ts
+++ src/deps.ts
@@ -1,3 +1,2 @@
 export { DB } from "https://deno.land/x/sqlite@v3.2.0/mod.ts";
 export type { RowObject } from "https://deno.land/x/sqlite@v3.2.0/mod.ts";
-export { Database } from "https://deno.land/x/sqlite3@0.5.3/mod.ts";
